56eab8451e0d0c9a2269c81a6a66744e74a00ae3
Jarimatti Valkonen <jarimatti@me.com>
2021-12-09 21:18:54 +0000

    Day 8 stub with failing test.

---
+++ lib/aoc2021/day8.ex
@@ -0,0 +1,13 @@
+defmodule Aoc2021.Day8 do
+  @moduledoc """
+  See https://adventofcode.com/2021/day/8
+  """
+
+  @spec solve_part1() :: non_neg_integer()
+  @spec solve_part1(Path.t()) :: non_neg_integer()
+  def solve_part1(_path \\ "priv/day8/input.txt") do
+    # stub
+    0
+  end
+
+end
---
+++ test/aoc2021/day8_test.exs
@@ -0,0 +1,10 @@
+defmodule Aoc2021.Day8Test do
+  use ExUnit.Case
+  doctest Aoc2021.Day8
+
+  alias Aoc2021.Day8
+
+  test "part 1 solution should be correct with test data" do
+    assert Day8.solve_part1("priv/day8/test-input.txt") == 26
+  end
+end
