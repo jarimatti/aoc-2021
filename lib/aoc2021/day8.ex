1b843e3cdbcd9fd142e4022bce8104a7036aee49
Jarimatti Valkonen <jarimatti@me.com>
2021-12-09 21:40:38 +0000

    Day 8, part 1 solved.

--- lib/aoc2021/day8.ex
+++ lib/aoc2021/day8.ex
@@ -5,9 +5,35 @@ defmodule Aoc2021.Day8 do
 
   @spec solve_part1() :: non_neg_integer()
   @spec solve_part1(Path.t()) :: non_neg_integer()
-  def solve_part1(_path \\ "priv/day8/input.txt") do
-    # stub
-    0
+  def solve_part1(path \\ "priv/day8/input.txt") do
+    path
+    |> read_input()
+    |> Enum.map(fn {_, v} -> v end)
+    |> Enum.map(fn v -> Enum.count(v, &is_easy_digit/1) end)
+    |> Enum.sum()
   end
 
+  defp read_input(path) do
+    path
+    |> File.stream!()
+    |> Stream.map(&String.trim/1)
+    |> Stream.reject(&empty_line?/1)
+    |> Stream.map(&parse_line/1)
+  end
+
+  defp empty_line?(""), do: true
+  defp empty_line?(_), do: false
+
+  defp parse_line(line) do
+    [a, b] =
+      line
+      |> String.split(" | ")
+      |> Enum.map(&String.split/1)
+
+    {a, b}
+  end
+
+  defp is_easy_digit(s) do
+    String.length(s) in [2, 3, 4, 7]
+  end
 end
--- test/aoc2021/day8_test.exs
+++ test/aoc2021/day8_test.exs
@@ -7,4 +7,8 @@ defmodule Aoc2021.Day8Test do
   test "part 1 solution should be correct with test data" do
     assert Day8.solve_part1("priv/day8/test-input.txt") == 26
   end
+
+  test "part 1 solution should be correct" do
+    assert Day8.solve_part1() == 365
+  end
 end
