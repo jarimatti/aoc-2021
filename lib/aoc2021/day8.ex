80974243d432072f48c53aec0466c03c5f11c4bf
Jarimatti Valkonen <jarimatti@me.com>
2021-12-09 23:13:41 +0000

    Day 8, part 2 solved.

--- lib/aoc2021/day8.ex
+++ lib/aoc2021/day8.ex
@@ -8,9 +8,91 @@ defmodule Aoc2021.Day8 do
   def solve_part1(path \\ "priv/day8/input.txt") do
     path
     |> read_input()
-    |> Enum.map(fn {_, v} -> v end)
-    |> Enum.map(fn v -> Enum.count(v, &is_easy_digit/1) end)
+    |> Stream.map(fn {_, v} -> v end)
+    |> Stream.map(fn v -> Enum.count(v, &is_easy_digit/1) end)
     |> Enum.sum()
+  end
+
+  @spec solve_part2() :: non_neg_integer()
+  @spec solve_part2(Path.t()) :: non_neg_integer()
+  def solve_part2(path \\ "priv/day8/input.txt") do
+    path
+    |> read_input()
+    |> Stream.map(&determine_number/1)
+    |> Enum.sum()
+  end
+
+  defp determine_number({a, b}) do
+    map = mappings(a)
+
+    b
+    |> Enum.map(fn x -> Map.get(map, x) end)
+    |> Integer.undigits()
+  end
+
+  defp mappings(list) do
+    {easy, complex} = Enum.split_with(list, &is_easy_digit/1)
+
+    easy_digits =
+      Enum.reduce(easy, %{}, fn d, acc ->
+        dd = map_easy_digit(d)
+        Map.put(acc, dd, d)
+      end)
+
+    complex
+    |> Enum.reduce(easy_digits, fn d, acc ->
+      dd = map_hard_digit(d, acc)
+      Map.put(acc, dd, d)
+    end)
+    |> swap_key_value()
+  end
+
+  defp swap_key_value(map) do
+    map
+    |> Map.to_list()
+    |> Enum.map(fn {k, v} -> {v, k} end)
+    |> Map.new()
+  end
+
+  defp map_easy_digit(d) do
+    case MapSet.size(d) do
+      2 -> 1
+      3 -> 7
+      4 -> 4
+      7 -> 8
+    end
+  end
+
+  defp map_hard_digit(d, map) do
+    case MapSet.size(d) do
+      5 ->
+        map_2_3_5(d, map)
+
+      6 ->
+        map_0_6_9(d, map)
+    end
+  end
+
+  defp map_2_3_5(d, map) do
+    diff_to_1 = MapSet.size(MapSet.difference(d, Map.get(map, 1)))
+    diff_to_4 = MapSet.size(MapSet.difference(d, Map.get(map, 4)))
+
+    case {diff_to_1, diff_to_4} do
+      {3, _} -> 3
+      {_, 2} -> 5
+      {_, 3} -> 2
+    end
+  end
+
+  defp map_0_6_9(d, map) do
+    diff_to_1 = MapSet.size(MapSet.difference(d, Map.get(map, 1)))
+    diff_to_4 = MapSet.size(MapSet.difference(d, Map.get(map, 4)))
+
+    case {diff_to_1, diff_to_4} do
+      {4, 3} -> 0
+      {5, 3} -> 6
+      {4, 2} -> 9
+    end
   end
 
   defp read_input(path) do
@@ -24,16 +106,30 @@ defmodule Aoc2021.Day8 do
   defp empty_line?(""), do: true
   defp empty_line?(_), do: false
 
+  def alphabet do
+    MapSet.new([:a, :b, :c, :d, :e, :f, :g])
+  end
+
   defp parse_line(line) do
     [a, b] =
       line
       |> String.split(" | ")
       |> Enum.map(&String.split/1)
 
-    {a, b}
+    sa = Enum.map(a, &parse_segment_set/1)
+    sb = Enum.map(b, &parse_segment_set/1)
+
+    {sa, sb}
+  end
+
+  defp parse_segment_set(word) do
+    word
+    |> String.graphemes()
+    |> Enum.map(&String.to_existing_atom/1)
+    |> MapSet.new()
   end
 
   defp is_easy_digit(s) do
-    String.length(s) in [2, 3, 4, 7]
+    MapSet.size(s) in [2, 3, 4, 7]
   end
 end
--- test/aoc2021/day8_test.exs
+++ test/aoc2021/day8_test.exs
@@ -11,4 +11,12 @@ defmodule Aoc2021.Day8Test do
   test "part 1 solution should be correct" do
     assert Day8.solve_part1() == 365
   end
+
+  test "part 2 solution should be correct with test data" do
+    assert Day8.solve_part2("priv/day8/test-input.txt") == 61_229
+  end
+
+  test "part 2 solution should be correct" do
+    assert Day8.solve_part2() == 975_706
+  end
 end
