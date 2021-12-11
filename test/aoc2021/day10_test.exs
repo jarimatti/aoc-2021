31b56b64ec8950c975b8081f9d63d37283623675
Jarimatti Valkonen <jarimatti@me.com>
2021-12-11 18:47:42 +0000

    Day 10 stub with failing test.

---
+++ test/aoc2021/day10_test.exs
@@ -0,0 +1,25 @@
+defmodule Aoc2021.Day10Test do
+  use ExUnit.Case
+  doctest Aoc2021.Day10
+
+  alias Aoc2021.Day10
+
+  test "part 1 solution should be correct with test data" do
+    assert Day10.solve_part1("priv/day10/test-input.txt") == 26_397
+  end
+
+  @tag :pending
+  test "part 1 solution should be correct" do
+    assert Day10.solve_part1() == -1
+  end
+
+  @tag :pending
+  test "part 2 solution should be correct with test data" do
+    assert Day10.solve_part2("priv/day10/test-input.txt") == -1
+  end
+
+  @tag :pending
+  test "part 2 solution should be correct" do
+    assert Day10.solve_part2() == -1
+  end
+end
