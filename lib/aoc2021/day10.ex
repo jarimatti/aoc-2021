42124da30ca22b011b71cf2dfbb571a840497b3b
Jarimatti Valkonen <jarimatti@me.com>
2021-12-12 15:23:46 +0000

    Day 10, part 1.

--- lib/aoc2021/day10.ex
+++ lib/aoc2021/day10.ex
@@ -3,12 +3,81 @@ defmodule Aoc2021.Day10 do
   See https://adventofcode.com/2021/day/10
   """
 
+  @illegal_scores %{
+    ?) => 3,
+    ?] => 57,
+    ?} => 1197,
+    ?> => 25_137
+  }
+
+  @type token() :: char()
+
+  defmodule Reader do
+    @moduledoc false
+
+    @spec read_input(Path.t()) :: [[Aoc2021.Day10.token()]]
+    def read_input(path) do
+      path
+      |> File.stream!()
+      |> Stream.map(&String.trim/1)
+      |> Stream.reject(&empty_line?/1)
+      |> Stream.map(&tokens/1)
+      |> Enum.to_list()
+    end
+
+    defp empty_line?(""), do: true
+    defp empty_line?(_), do: false
+
+    defp tokens(line) do
+      String.to_charlist(line)
+    end
+  end
+
+  defmodule Parser do
+    @moduledoc false
+
+    @spec parse([Aoc2021.Day10.token()]) ::
+            :ok
+            | :incomplete
+            | {:error, {:unexpected_token, Aoc2021.Day10.token(), non_neg_integer()}}
+    def parse(line) do
+      result =
+        line
+        |> Enum.with_index()
+        |> Enum.reduce_while([], &step/2)
+
+      case result do
+        [] -> :ok
+        [_ | _] -> :incomplete
+        {:error, reason} -> {:error, reason}
+      end
+    end
+
+    defp step({token, _pos}, stack) when token in [?(, ?{, ?[, ?<],
+      do: {:cont, [token | stack]}
+
+    defp step({?), _pos}, [?( | rest]), do: {:cont, rest}
+    defp step({?}, _pos}, [?{ | rest]), do: {:cont, rest}
+    defp step({?], _pos}, [?[ | rest]), do: {:cont, rest}
+    defp step({?>, _pos}, [?< | rest]), do: {:cont, rest}
+    defp step({token, pos}, _stack), do: {:halt, {:error, {:unexpected_token, token, pos}}}
+  end
+
   @spec solve_part1() :: non_neg_integer()
   @spec solve_part1(Path.t()) :: non_neg_integer()
-  def solve_part1(_path \\ "priv/day10/input.txt") do
-    # stub
-    0
+  def solve_part1(path \\ "priv/day10/input.txt") do
+    path
+    |> Reader.read_input()
+    |> Enum.map(&Parser.parse/1)
+    |> Enum.map(&error_score/1)
+    |> Enum.sum()
   end
+
+  defp error_score({:error, {:unexpected_token, token, _pos}}) do
+    Map.get(@illegal_scores, token, 0)
+  end
+
+  defp error_score(_), do: 0
 
   @spec solve_part2() :: non_neg_integer()
   @spec solve_part2(Path.t()) :: non_neg_integer()
--- test/aoc2021/day10_test.exs
+++ test/aoc2021/day10_test.exs
@@ -8,9 +8,8 @@ defmodule Aoc2021.Day10Test do
     assert Day10.solve_part1("priv/day10/test-input.txt") == 26_397
   end
 
-  @tag :pending
   test "part 1 solution should be correct" do
-    assert Day10.solve_part1() == -1
+    assert Day10.solve_part1() == 462_693
   end
 
   @tag :pending
