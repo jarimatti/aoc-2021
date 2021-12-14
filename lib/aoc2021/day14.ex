c0095edc4560b9980f3f701b40d3b2c8ab768a42
Jarimatti Valkonen <jarimatti@me.com>
2021-12-14 21:12:30 +0000

    Day 14, part 2 solved.
    Had to look up another solutions for part 2.

--- lib/aoc2021/day14.ex
+++ lib/aoc2021/day14.ex
@@ -3,10 +3,13 @@ defmodule Aoc2021.Day14 do
   See https://adventofcode.com/2021/day/14
   """
 
+  @type template() :: charlist()
+  @type charmap() :: %{charlist() => charlist()}
+
   defmodule Reader do
     @moduledoc false
 
-    @spec read_input(Path.t()) :: {charlist(), %{charlist() => charlist()}}
+    @spec read_input(Path.t()) :: {Aoc2021.Day14.template(), Aoc2021.Day14.charmap()}
     def read_input(path) do
       [start, "" | rest] =
         path
@@ -33,17 +36,26 @@ defmodule Aoc2021.Day14 do
   def solve_part1(path \\ "priv/day14/input.txt") do
     {start, map} = Reader.read_input(path)
 
-    {{least, _}, {most, _}} =
-      Enum.reduce(1..10, start, fn _, acc -> step(acc, map) end)
-      |> Enum.frequencies()
-      |> Enum.map(fn {a, b} -> {b, a} end)
-      |> Enum.sort()
-      |> Enum.min_max()
-
-    most - least
+    solve_iteration(10, start, map)
   end
 
-  @spec step(charlist(), %{charlist() => charlist()}) :: charlist()
+  @spec solve(non_neg_integer(), template(), charmap()) :: non_neg_integer()
+  def solve(n, start, map) do
+    # This a straight-forward solution and it works for part 1, but for part 2 the length of the list explodes.
+    # However we can process this depth-first, taking each pair and running them through all steps, then proceeding to next pair.
+    # Given we're only interested in the counts, we can just track a map of the counts and don't need to keep the whole list in memory.
+    #
+    # Scratch that, the depth-first also grows exponentially.
+    #
+    # Had to look up other solutions and they were much better: instead of recursing, iterate over the pair counts.
+
+    1..n
+    |> Enum.reduce(start, fn _, acc -> step(acc, map) end)
+    |> Enum.frequencies()
+    |> score()
+  end
+
+  @spec step(template(), charmap()) :: template()
   def step(s, map) do
     s
     |> Enum.chunk_every(2, 1)
@@ -57,10 +69,81 @@ defmodule Aoc2021.Day14 do
     end)
   end
 
+  def step_recursion(s, map, i) do
+    s
+    |> Enum.chunk_every(2, 1)
+    |> Enum.reduce(%{}, fn
+      [_a, _b] = s, acc ->
+        step_recursion(s, map, i, acc)
+
+      [c], acc ->
+        add_count(acc, c)
+    end)
+    |> score()
+  end
+
+  defp score(freqs) do
+    {min, max} =
+      freqs
+      |> Map.values()
+      |> Enum.min_max()
+
+    max - min
+  end
+
+  defp add_count(acc, c), do: Map.update(acc, c, 1, fn x -> x + 1 end)
+
+  @spec step_recursion(template(), charmap(), non_neg_integer(), %{char() => non_neg_integer()}) ::
+          %{
+            char() => non_neg_integer()
+          }
+  def step_recursion([a, _b], _map, 0, counts) do
+    add_count(counts, a)
+  end
+
+  def step_recursion([a, b] = s, map, i, counts) when i > 0 do
+    c = Map.get(map, s)
+
+    counts_1 = step_recursion([a, c], map, i - 1, counts)
+    counts_2 = step_recursion([c, b], map, i - 1, counts_1)
+
+    counts_2
+  end
+
+  def step_iteration(freqs, map) do
+    freqs
+    |> Enum.flat_map(fn
+      {[a, b] = s, count} ->
+        c = Map.get(map, s)
+        [{[a, c], count}, {[c, b], count}]
+
+      c ->
+        [c]
+    end)
+    |> Enum.reduce(%{}, fn
+      {l, count}, acc ->
+        Map.update(acc, l, count, fn x -> x + count end)
+      end)
+  end
+
+  def solve_iteration(n, s, map) do
+    s
+    |> Enum.chunk_every(2, 1)
+    |> Enum.frequencies()
+    |> Stream.iterate(&step_iteration(&1, map))
+    |> Enum.at(n)
+    |> Enum.reduce(%{}, fn
+      {c, count}, acc ->
+        Map.update(acc, hd(c), count, fn x -> x + count end)
+      end)
+    |> score()
+  end
+
   @spec solve_part2() :: non_neg_integer()
   @spec solve_part2(Path.t()) :: non_neg_integer()
-  def solve_part2(_path \\ "priv/day14/input.txt") do
-    # stub
-    0
+  def solve_part2(path \\ "priv/day14/input.txt") do
+    {start, map} = Reader.read_input(path)
+
+    solve_iteration(40, start, map)
   end
 end
--- test/aoc2021/day14_test.exs
+++ test/aoc2021/day14_test.exs
@@ -8,29 +8,18 @@ defmodule Aoc2021.Day14Test do
     assert Day14.solve_part1("priv/day14/test-input.txt") == 1588
   end
 
-  test "part 1 step works" do
-    {s, map} = Day14.Reader.read_input("priv/day14/test-input.txt")
-
-    s = Day14.step(s, map)
-    assert s == 'NCNBCHB'
-
-    s = Day14.step(s, map)
-    assert s == 'NBCCNBBBCBHCB'
-
-    s = Day14.step(s, map)
-    assert s == 'NBBBCNCCNBBNBNBBCHBHHBCHB'
-
-    s = Day14.step(s, map)
-    assert s == 'NBBNBNBBCCNBCNCCNBBNBBNBBBNBBNBBCBHCBHHNHCBBCBHCB'
-  end
-
   test "part 1 solution should be correct" do
     assert Day14.solve_part1() == 3697
   end
 
-  @tag :pending
+  test "solve_iteration produces same result" do
+    {s, map} = Day14.Reader.read_input("priv/day14/test-input.txt")
+
+    assert Day14.solve_iteration(10, s, map) == 1588
+  end
+
   test "part 2 solution should be correct with test data" do
-    assert Day14.solve_part2("priv/day14/test-input.txt") == -1
+    assert Day14.solve_part2("priv/day14/test-input.txt") == 2_188_189_693_529
   end
 
   @tag :pending
